bae7ea4dd45dc79f2895c87204c34a2408134958
Lalasimon <lalaszymon@gmail.com>
2023-04-06 10:06:51 +0200

    mobile nav with rwd and routes)

--- FightClub/src/App.tsx
+++ FightClub/src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Coaches } from "./pages/Coaches";
-import { Nav } from "./components/nav";
+import { Nav } from "./components/Nav";
 const App = () => {
     return (
         <>
